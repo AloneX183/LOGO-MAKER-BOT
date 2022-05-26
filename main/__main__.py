413d6d8a5cf0941506134fd3a5ec3536d199fcbb
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-26 16:46:41 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -8,7 +8,7 @@ from main.logo import generate_logo
 START = """
 **🔮 Hello There, You Can Use Me To Create Awesome Logos...**
 
-➤ Click /help Or The Button Below To Know How To Use Me
+➤ Click The Button Below To Know How To Use Me
 """
 
 HELP = """
@@ -18,18 +18,18 @@ HELP = """
 **To Make Square Logo - ** `/logosq Your Name`
 
 **♻️ Example:** 
-`/logo TechZBots`
-`/logosq TechZBots`
+`/logo Beta`
+`/logosq BetaBots`
 """
 
 # Commands
 @app.on_message(filters.command("start"))
 async def start(bot, message):
-  await message.reply_photo("https://telegra.ph/file/7a98ead33e7b99fd82cc7.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu"), InlineKeyboardButton(text="Repo", url="https://github.com/TechShreyash/TechZ-Logo-Maker-Bot")]]))
+  await message.reply_photo("https://telegra.ph//file/69b6154eaecdaf3845d9f.jpg",caption=START,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Help", callback_data="help_menu"), InlineKeyboardButton(text="Repo", url="https://github.com/TechShreyash/TechZ-Logo-Maker-Bot")]]))
 
 @app.on_message(filters.command("help"))
 async def help(bot, message):
-  await message.reply_photo("https://telegra.ph/file/7a98ead33e7b99fd82cc7.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
+  await message.reply_photo("https://telegra.ph//file/69b6154eaecdaf3845d9f.jpg",caption=HELP,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data="start_menu")]]))
 
 @app.on_message(filters.command("logo") & filters.incoming & filters.text & ~filters.forwarded & (
   filters.group | filters.private))
