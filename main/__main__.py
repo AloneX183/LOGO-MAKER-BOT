dbf624a27443c43438cc56a2d6504c99a2a580fa
Lolan <102511853+Jeolpaul@users.noreply.github.com>
2022-05-26 21:07:27 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -136,7 +136,7 @@ async def broadcast(bot, message):
      	pass
 
 @app.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
-async def get_users(client: Client, message: Message):    
+async def get_users(app: app, message: Message):    
     msg = await client.send_message(chat_id=message.chat.id, text="അവിടെ നില്ല് ഇപ്പോ തരാം 🤨")
     ids = getid()
     tot = len(ids)
